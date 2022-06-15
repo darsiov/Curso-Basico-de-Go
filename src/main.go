c080de686690db85cf2e0997bc8969519a2f8e2a
Daniel Arturo Silva Oviedo <arturoviedo0630@gmail.com>
2022-06-15 18:50:09 -0300

    Repaso de los operadores aritmeticos de Go

--- src/main.go
+++ src/main.go
@@ -3,7 +3,45 @@ package main
 import "fmt"
 
 func main() {
+	// Area de un cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
 
-	fmt.Println("Hola mundo")
+	fmt.Println("Area cuadrado: ", areaCuadrado)
 
+	x := 10
+	y := 50
+
+	//Suma
+	result := x + y
+	fmt.Println("Suma:", result)
+
+	//Resta
+
+	result = y - x
+	fmt.Println("Resta:", result)
+
+	//Multiplicación
+
+	result = x * y
+	fmt.Println("Multiplicacion:", result)
+
+	//Division
+
+	result = y / x
+	fmt.Println("Division:", result)
+
+	// Modulo
+	result = y % x
+	fmt.Println("Residuo:", result)
+
+	// Incremental
+	x++
+	fmt.Println("Incremental:", x)
+
+	//Decremental
+	x--
+	fmt.Println("Decremental:", x)
+
+	//Retos, calcular el area de un trapecio y un circulo
 }
