b7d4352c6d9bd9b037b1d786ee65a2c40149f1d3
Daniel Arturo Silva Oviedo <arturoviedo0630@gmail.com>
2022-07-06 14:17:04 -0500

    Sintaxis de los diferentes tipos de ciclo en Golang (solo es for con variantes)

--- src/main.go
+++ src/main.go
@@ -1,31 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
-func areaTrapecio(a, b, h float64) float64 {
-	area := ((a + b) / 2) * h
-	return area
-}
-func areaCirculo(r float64) float64 {
-	area := math.Pi * r * r
-	return area
-}
-func separador() {
-	fmt.Println("-----------------------------------------------------------------------")
-}
+import "fmt"
 
 func main() {
-	var a, b, h float64 = 14, 20, 5
-	var r float64 = 4
-	aT := areaTrapecio(a, b, h)
-	aC := areaCirculo(r)
-	fmt.Println("Medidas del trapecio:  a, b, h = (", a, ",", b, ",", h, ") cm")
-	fmt.Println("Área del trapecio:", aT, "cm^2")
-	separador()
-	fmt.Println("Radio del circulo", r, "cm")
-	fmt.Println("Área del ciculo:", aC, "cm^2")
 
+	//For condicional
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
+	fmt.Printf("\n")
+	// For while
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+	// For forever
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
 }
